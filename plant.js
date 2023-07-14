b84d6f5c69c8b27273111ff24eb0ae215cf1688b
Hyakuren <kenichigomi.inferno@gmail.com>
2023-07-14 01:16:25 -0400

    Update plant.js

--- plant.js
+++ plant.js
@@ -41,9 +41,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+var brownColor = getRandomInt(64, 168)
+
 function draw(startX, startY, len, angle, branchWidth) {
     ctx.lineWidth = branchWidth;
-    var brownColor = getRandomInt(64, 168)
 
     ctx.beginPath();
     ctx.save();
